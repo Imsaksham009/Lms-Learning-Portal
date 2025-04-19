274e45284d2fa906ad63a1f182f5c602ee0ee062
Imsaksham009 <guptasaksham82@gmail.com>
2025-04-19 10:52:33 +0530

    Enhances app security and middleware setup
    - Adds security middlewares: rate limiting, Helmet, and HPP
    - Expands CORS configuration with additional methods
    - Improves route handling with a clear 404 response
    
    - Supports better security and API usability

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,9 @@ require("dotenv").config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
+import hpp from "hpp";
 import morgan from "morgan";
 import { catchAsync } from "./Errors/catchAsync";
 import { AppError } from "./Errors/errorHandler";
@@ -9,15 +12,33 @@ import { errorMiddleWare } from "./Middlewares/errorMiddleware";
 import userRoute from "./Routes/userRoute";
 const app = express();
 
+//security middlewares
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
+app.use(hpp());
+app.use(helmet());
+app.use(limiter);
+
+//logger
 process.env.NODE_ENV === "development" && app.use(morgan("dev"));
+
+//parsers
 app.use(cookieParser());
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true }));
+
+//cors
 app.use(
 	cors({
 		origin: process.env.CLIENT_URL,
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 	})
 );
+
+//test api- will be removed later
 app.get(
 	"/test",
 	catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -25,9 +46,10 @@ app.get(
 	})
 );
 
+//routes
 app.use("/api/v1", userRoute);
 
-//unmatched routes
+//unmatched routes - 404 BAD REQUEST
 app.get("*", (req: Request, res: Response) => {
 	res.status(404).json({
 		message: "Route not found",
